7b0a0ea1112be5015a8390cc8da36cb063e0fa1e
Nicasio <nicasiomontes97@gmail.com>
2019-06-19 12:54:40 -0300

    no relevant modifictions

--- query_generator.py
+++ query_generator.py
@@ -4,7 +4,9 @@ import scraping_imdb
 rec = requests.get('https://www.imdb.com/title/tt0110912/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd' + 
 	'-322d-4646-8962-327b42fe94b1&pf_rd_r=QGSH8PWCFZMYZZ6JJGDX&pf_rd_s=center-1&pf_rd_t=15506&pf_' + 
 	'rd_i=top&ref_=chttp_tt_8')
-Main_Soup = bs4.BeautifulSoup(rec.content,'html.parser') 
+Main_Soup = bs4.BeautifulSoup(rec.content,'html.parser')
+recSec = requests.get('https://www.imdb.com/title/tt4154796/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=ea4e08e1-c8a3-47b5-ac3a-75026647c16e&pf_rd_r=DG1G7TR5BQM8GNQBRM8Z&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=moviemeter&ref_=chtmvm_tt_4')
+Secondary_Soup = bs4.BeautifulSoup(recSec.content,'html.parser')	 
 # rec.content = rec.text
 
 def insert_movie(soup,movieName=None):
@@ -110,7 +112,6 @@ def print_all_queries(soup):
 		+" "+"\n"+relate_MovieCategory(soup,Name,category_list)+"\n"+" "+"\n"+relate_MovieActor(soup,Name,actor_list))
 	
 print_all_queries(Main_Soup)
-
 
 
 
